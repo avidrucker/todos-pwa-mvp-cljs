783dae72202bdc7785b3dfba8cd84a82b3c4e13a
avidrucker <avidrucker@gmail.com>
2023-08-04 15:02:24 -0400

    add t-index to app, todos

--- src/main/my_todo_app/core.cljs
+++ src/main/my_todo_app/core.cljs
@@ -2,11 +2,23 @@
   (:require [reagent.core :as reagent]
             [reagent.dom :as reagent-dom]))
 
+(defn save-cntr [t-index]
+  (let [t-index-js (js/JSON.stringify (clj->js {:t-index t-index}))]
+    (js/localStorage.setItem "t-index" t-index-js)))
+
 (defn save-todos [todos]
   (let [todos-js (js/JSON.stringify (clj->js {:todos todos}))]
-    ;; (println "Saving todos:" todos-js) ; Debugging line
     (js/localStorage.setItem "todos" todos-js)))
 
+(defn load-cntr []
+  (try (let [saved-cntr (js/localStorage.getItem "t-index")
+             loaded-cntr (-> saved-cntr
+                             js/JSON.parse
+                             (js->clj :keywordize-keys true)
+                             :t-index)]
+         (if loaded-cntr loaded-cntr 0)) (catch :default e
+                          (js/console.error "Error loading counter" e)
+                          0)))
 
 (defn load-todos []
   (try
@@ -22,27 +34,55 @@
       (js/console.error "Error loading todos" e)
       [])))
 
-
 (defonce app-state
   (reagent/atom
-   {:todos (load-todos)}))
+   {:todos (load-todos)
+    :t-index (load-cntr)})) ; Initialize t-index counter
+
 
 (add-watch app-state :save-todos
   (fn [_ _ _ new-state]
     (save-todos (:todos new-state))))
 
+(add-watch app-state :save-cntr
+  (fn [_ _ _ new-state]
+    (save-cntr (:t-index new-state))))
+
 (defn todo-item [todo]
+  [:li
+   [:span "T-Index: " (:t-index todo) " - "] ; Display t-index
+   [:input {:type "checkbox" :checked (:done todo)
+            :on-change #(swap! app-state update-in [:todos (:id todo)] assoc :done (not (:done todo)))}]
+   [:span (:text todo)]
+   [:button {:on-click #(swap! app-state update :todos (fn [todos] (remove (fn [x] (= (:id todo) (:id x))) todos)))} "Delete"]])
+
+
+#_(defn todo-item [todo]
   [:li
    [:input {:type "checkbox" :checked (:done todo)
             :on-change #(swap! app-state update-in [:todos (:id todo)] assoc :done (not (:done todo)))}]
    [:span (:text todo)]
    [:button {:on-click #(swap! app-state update :todos (fn [todos] (remove (fn [x] (= (:id todo) (:id x))) todos)))} "Delete"]])
 
+(defn cntr []
+  [:p (str "The counter is at " (:t-index @app-state))])
+
 (defn todo-list []
-  [:ul (for [todo (:todos @app-state)] ^{:key (:id todo)} [todo-item todo])])
+  [:ul (for [todo (:todos @app-state)]
+         ^{:key (:id todo)} [todo-item todo])])
 
 (defn add-todo [text]
-  (swap! app-state update :todos conj {:id (random-uuid) :text text :done false}))
+  (swap! app-state
+         (fn [state]
+           (let [current-index (:t-index state)]
+             (-> state
+                 (update :todos conj
+                         {:id (random-uuid)
+                          :text text
+                          :done false
+                          :t-index current-index})
+                 (assoc :t-index (inc current-index)))))))
+
 
 (defn todo-input []
   (let [input-ref (reagent/atom nil)]
@@ -56,6 +96,7 @@
 
 (defn main-panel []
   [:div
+   [cntr]
    [todo-input]
    [todo-list]])
 
